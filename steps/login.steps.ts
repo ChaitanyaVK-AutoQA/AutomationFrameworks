db65ce082ca9c1c4d5830a70e428e1c2548cfa40
ChaitanyaVK-AutoQA <cvk.oct31@gmail.com>
2025-09-23 20:33:03 +0530

    space

--- steps/login.steps.ts
+++ steps/login.steps.ts
@@ -3,7 +3,7 @@ import { expect } from "@playwright/test";
 import { CustomWorld } from "../support/world"; 
 import { LoginPage } from "../pages/LoginPage";
 import { LandingPage } from "../pages/LandingPage";
-import { assert } from "console";
+
 
 let loginPage: LoginPage;
 let landingPage: LandingPage;
@@ -21,16 +21,14 @@ Then("I should see the homepage", async function (this: CustomWorld) {
   await expect(this.page.locator(".header_label")).toHaveText("Swag Labs");
   landingPage = new LandingPage(this.page);
   await landingPage.clickburgerButton();
-  await landingPage.clickcloseButton();
- 
-  
+  await landingPage.clickcloseButton();  
 });
 
 Then('I validate in the add cart page', async function (this: CustomWorld) {
           landingPage = new LandingPage(this.page);
          await landingPage.clickbagtoCart();
-  await landingPage.clickCarticon();
- const errorText = await landingPage.getElementText();
-  console.log("errorText",errorText);
+         await landingPage.clickCarticon();
+         const errorText = await landingPage.getElementText();
+         console.log("errorText",errorText);
         
         })
